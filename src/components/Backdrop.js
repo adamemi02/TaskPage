9f8c6e99ba5a42e7bf31575a76fa0200e5000301
Madalina Tocitu <madalina.tocitu@radcom.com>
2022-06-29 15:28:33 +0300

    [new-branch-name] Some changes {2022-06-29 12:28}

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,24 +1,15 @@
 import React from "react";
 import classes from "./Backdrop.module.css";
 import Modal from "./Modal.js";
+
 function Backdrop(props){
     return(
 
         <div className={classes.Backdrop} onClick={props.onCancel}>
             <Modal />
         </div>
-
     );
 
 
 }
 export default Backdrop;
-
-// add an empty line before defining the component
-
-// the className can be renamed to className="backdrop", same name as the component,
-// but not uppercased
-
-// rename the css file and import it like: import './backdrop.css'.
-// same for the other js files, there's no need to give a name to the css file you import and
-// then to use it in the className
